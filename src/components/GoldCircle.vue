e3105b265f62e7c02085be5eb8abfd4c384f8ab1
azka1415 <rafifzaydan4@gmail.com>
2022-12-27 18:50:58 +0700

    created circle components

---
+++ src/components/GoldCircle.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg
+		width="18"
+		height="18"
+		viewBox="0 0 18 18"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		role="img"
+	>
+		<circle
+			cx="9"
+			cy="9"
+			r="9"
+			fill="#e9b067"
+		/>
+	</svg>
+</template>
+<script>
+export default {
+	name: "GoldCircle",
+};
+</script>
+<style lang=""></style>
